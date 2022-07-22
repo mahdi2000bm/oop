abb35def7f0d08c877ad8eefe0aa7ab2b1925dcc
mhdibaqri <mahdi2000bm@gmail.com>
2022-07-22 21:16:19 +0430

    add conn

--- 04-Inheritance.php
+++ 04-Inheritance.php
@@ -1,4 +1,15 @@
 <?php 
     class BaseSearch {
-        
+        private $db;
+        public function __construct(){
+
+            $conn =  "mysql:dbname='livemap_business';host='localhost'";
+            try {
+                $connect = new PDO($conn , "root", "");
+                echo "OK!";
+            } catch (PDOException $result_e) {
+                die($result_e);
+            }
+        } 
     }
+    $dbConn = new BaseSearch();
