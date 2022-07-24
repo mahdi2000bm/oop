7469c6293eb4485f215e9586b8b88ed0db62a7a2
mhdibaqri <mahdi2000bm@gmail.com>
2022-07-24 19:09:39 +0430

    init abstract Class

--- 05-abstractionClass.php
+++ 05-abstractionClass.php
@@ -1,3 +1,17 @@
 <?php
-
-    
+    abstract class baseCar
+    {   
+        protected string $car;
+        public function __cunstract($name)
+        {
+            $this->$car = $name;
+        }
+        abstract protected function carOption();
+    }
+    class Bmw extends baseCar
+    {
+        protected function carOption(){
+            return "this car in $this->name";
+        }
+    }
+    $BMW = new Bmw("BMW");
